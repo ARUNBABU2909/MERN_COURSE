fc3e708270d45388b133a8d90bcfec9564462921
arunbabu <arunbabutm55@gmail.com>
2025-11-01 09:50:48 +0530

    first commit

---
+++ Javascript/javascript.js
@@ -0,0 +1,20 @@
+let a=2
+let b=21
+let c=30
+document.write("<br/> the sum of add:",a+b)
+document.write("<br/> the sum of add:",a-b)
+document.write("<br/> the sum of add:",a*b)
+document.write("<br/> the sum of add:",a/b)
+document.write("<br/> the sum of add:",a%b)
+let names=['arun','babu','dharun']
+ document.writeln("<br/>",names[0])
+ document.writeln("<br/>",a+b+c/3)
+ let lenghth=a
+ let width=b
+ document.writeln("<br/> This is a area of a rectangle:",lenghth*width )
+ 
+ f=(c*9/5)+32
+ document.writeln("<br/> this celcius to farenhit:",f)
+ document.writeln("<br/> this is remainder:",b%a)
+ document.writeln("<br/> this inrement after a:",a+=20)
+ document.writeln("<br/> this inrement after a:",a*=5)
